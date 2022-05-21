d2d74b8619d1fc0fc013fecd77e3f7311ca01484
Sven Svensson <sven@bringsrud.net>
2022-05-21 12:41:20 +0200

    Initial Commit

---
+++ ObservedEnumsWithAssociatedValues/ObservedEnumsWithAssociatedValuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedEnumsWithAssociatedValuesApp.swift
+//  ObservedEnumsWithAssociatedValues
+//
+//  Created by Sven Svensson on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ObservedEnumsWithAssociatedValuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
